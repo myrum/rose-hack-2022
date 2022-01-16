bd2703232ebb27c9a5ad59fce58cbaccd44c4380
Myriam Aviles <myriampaviles@berkeley.edu>
2022-01-15 16:18:22 -0800

    adding meals table :-1:

--- db/migrate/20220115234858_add_cols_to_meals.rb
+++ db/migrate/20220115234858_add_cols_to_meals.rb
@@ -1,4 +1,9 @@
 class AddColsToMeals < ActiveRecord::Migration[7.0]
   def change
+    create_table :meals do |t|
+      t.string :title
+      t.string :type
+      t.text :description
+    end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+meals = [{:title => 'Aladdin', :type => 'food', :description => "bla bla bla bla"} 
+]
+
+meals.each do |meal|
+Meals.create!(meal)
+end
