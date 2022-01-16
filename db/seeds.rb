c3b1805e32992308b2938cba67a074b33b326cdb
Myriam Aviles <myriampaviles@berkeley.edu>
2022-01-15 17:34:19 -0800

    updated index to be correct

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,4 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-recipes = Recipe.create([{title: 'food', category: 'dinner', info: "bla bla bla bla"} 
-])
+recipes = Recipe.create!([{ title: 'food', category: 'dinner', info: "bla bla bla bla" } ])
